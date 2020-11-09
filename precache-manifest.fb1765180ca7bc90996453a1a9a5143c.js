6f1825c64a858d638a6b2709c9d0b93e43b5ad58
flame1800 <r.shaficoff@gmail.com>
2020-11-09 17:31:48 +0300

    Updates

---
+++ precache-manifest.fb1765180ca7bc90996453a1a9a5143c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1aabbae40041c11e18df82c78a7e68",
+    "url": "/shedule-spk/index.html"
+  },
+  {
+    "revision": "ae6855d299f683e9db05",
+    "url": "/shedule-spk/static/css/main.e2c24769.chunk.css"
+  },
+  {
+    "revision": "d3e322ed5fab4b4c0196",
+    "url": "/shedule-spk/static/js/2.7456db0a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/shedule-spk/static/js/2.7456db0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6855d299f683e9db05",
+    "url": "/shedule-spk/static/js/main.b78cf0f6.chunk.js"
+  },
+  {
+    "revision": "cbf58e185dce66155316",
+    "url": "/shedule-spk/static/js/runtime-main.3830deaa.js"
+  }
+]);
