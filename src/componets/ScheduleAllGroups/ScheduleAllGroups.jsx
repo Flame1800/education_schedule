c62fc1c0188e1ea6d6c1a6f8b1338ebec4860e66
flame1800 <ruslan08022201@gmail.com>
2022-11-17 17:23:09 +0500

    add cabs on day

--- src/componets/ScheduleAllGroups/ScheduleAllGroups.jsx
+++ src/componets/ScheduleAllGroups/ScheduleAllGroups.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './sheduleAllGroups.scss';
 import {observer} from "mobx-react-lite";
 import scheduleStore from "../../store/scheduleStore";
 import WeekLesson from "../Lesson/WeekLesson/WeekLesson";
 import filterLessons from "../../utils/filterLessons";
-import {DateTime} from "luxon";
 import _ from "lodash";
-import {SwiperSlide} from 'swiper/react';
 import 'swiper/swiper.scss';
 import filterStore from "../../store/filterStore";
 import LessonsSlider from "./LessonsSlider";
+import NawWeek from "../NawWeek/NawWeek";
+import datesStore from "../../store/datesStore";
 
 function ScheduleAllGroups() {
     const {currLessons} = scheduleStore
     const {mode} = filterStore
+    const {currDay} = datesStore
+    const [dayLessons, setDayLessons] = useState([])
 
+    useEffect(() => {
+        const value = mode === "allGroups" ? "group.name" : "cabinet.number";
+
+        const lessonsToday = _.sortBy(currLessons.filter(lesson => lesson.date === currDay), value)
+        const groupLessons = _.groupBy(lessonsToday, value)
+        const lessonsGroupPairs = Object.entries(groupLessons)
+
+        setDayLessons(lessonsGroupPairs)
+    }, [currDay, currLessons, mode])
 
     const generateLessons = (dayLessons) => {
         const fLessons = filterLessons(dayLessons);
         return fLessons.map((lesson) => <WeekLesson key={lesson._id} lesson={lesson}/>);
     }
 
-    const value = mode === "allGroups" ? "group.name" : "cabinet.number";
 
-    const today = DateTime.now().toISODate()
-    const lessonsToday = _.sortBy(currLessons.filter(lesson => lesson.date === today), value)
-    const groupLessons = _.groupBy(lessonsToday, value)
-    const lessonsGroupPairs = Object.entries(groupLessons)
-
-    const firstHalf = lessonsGroupPairs.filter((_, i) => i <= lessonsGroupPairs.length / 2)
-    const secondHalf = lessonsGroupPairs.filter((_, i) => i >= lessonsGroupPairs.length / 2)
+    const firstHalf = dayLessons.filter((_, i) => i <= dayLessons.length / 2)
+    const secondHalf = dayLessons.filter((_, i) => i >= dayLessons.length / 2)
 
 
     if (mode === 'cabs') {
@@ -37,25 +42,27 @@ function ScheduleAllGroups() {
             <div className='container-all'>
                 <div className="schedule-all">
                     <div className="cabs">
-                        {lessonsGroupPairs.map(pair => {
-                            const [groupName, groupLessons] = pair
-                            const groupNameComponent = <div
-                                className="group">{groupName === "undefined" ? "***" : groupName}</div>
-                            console.log(groupName)
+                        <NawWeek/>
+                        <div className="cab-items">
+                            {dayLessons.map(pair => {
+                                const [groupName, groupLessons] = pair
+                                const groupNameComponent = <div
+                                    className="group">{groupName === "undefined" ? "***" : groupName}</div>
 
-                            return (
-                                <div className="container-day">
-                                    <div className="row-items">
-                                        <div className="head">
-                                            {groupNameComponent}
-                                        </div>
-                                        <div className="lesson-cont">
-                                            {generateLessons(groupLessons)}
+                                return (
+                                    <div className="container-day">
+                                        <div className="row-items">
+                                            <div className="head">
+                                                {groupNameComponent}
+                                            </div>
+                                            <div className="lesson-cont">
+                                                {generateLessons(groupLessons)}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
