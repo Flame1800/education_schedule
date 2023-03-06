ea40119008e8378dc1cd0bbc1dde2b34f303dee5
flame1800 <ruslan08022201@gmail.com>
2023-03-06 15:10:45 +0500

    fix bug duble lesson

--- src/componets/Views/Schedule/SheduleDay/SheduleDay.js
+++ src/componets/Views/Schedule/SheduleDay/SheduleDay.js
@@ -1,49 +1,55 @@
 import React from "react";
 import "./sheduleDay.scss";
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import NavWeek from "../../../NawWeek/NawWeek";
 import datesStore from "../../../../store/datesStore";
 import DayLesson from "../../../Lesson/DayLesson/DayLesson";
-import {Lessons, Main, ScheduleWrapper} from "./ScheduleDay.style";
+import { Lessons, Main, ScheduleWrapper } from "./ScheduleDay.style";
 import NoLessonsTitle from "../../../Common/NoLessonsTitle";
 import fillEmptyLessons from "../../../../lib/fillEmptyLessons";
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
 import TalksPromoInfo from "../../../Lesson/WeekLesson/TalksPromoInfo";
 
+function ScheduleDay({ lessons }) {
+  const { currDay } = datesStore;
 
-function ScheduleDay({lessons}) {
-    const {currDay} = datesStore
+  const filterLessonsByDay = (lessons) =>
+    lessons.filter((lesson) => lesson.date === currDay);
+  const dayLessons = filterLessonsByDay(lessons);
 
-    const filterLessonsByDay = (lessons) => lessons.filter((lesson) => lesson.date === currDay)
-    const dayLessons = filterLessonsByDay(lessons)
+  return (
+    <ScheduleWrapper>
+      {lessons.length !== 0 && <NavWeek />}
+      <Main>
+        <Lessons>
+          {dayLessons.length === 0 ? (
+            <NoLessonsTitle>Нет пар</NoLessonsTitle>
+          ) : (
+            fillEmptyLessons(dayLessons, DateTime.fromISO(currDay)).map(
+              (lesson, i) => {
+                const isDouble = Array.isArray(lesson);
+                const tempLesson = isDouble ? lesson[0] : lesson;
 
+                const isMonday = DateTime.fromISO(currDay).weekday === 1;
+                const isTalks =
+                  tempLesson.lessonNumber === 1 ||
+                  tempLesson.lessonNumber === 4;
 
-    return (
-        <ScheduleWrapper>
-            {lessons.length !== 0 && <NavWeek/>}
-            <Main>
-                <Lessons>
-                    {dayLessons.length === 0
-                        ? <NoLessonsTitle>Нет пар</NoLessonsTitle>
-                        : fillEmptyLessons(dayLessons, DateTime.fromISO(currDay))
-                            .map((lesson, i) => {
-                                const isDouble = Array.isArray(lesson)
-                                const tempLesson = isDouble ? lesson[0] : lesson
-
-                                const isMonday = DateTime.fromISO(currDay).weekday === 1
-                                const isTalks = tempLesson.lessonNumber === 1 || tempLesson.lessonNumber === 4
-
-                                return (
-                                    <>
-                                        {isTalks && isMonday && <TalksPromoInfo rounded pair={tempLesson.lessonNumber}/>}
-                                        <DayLesson key={i} lesson={lesson}/>
-                                    </>
-                                )
-                            })}
-                </Lessons>
-            </Main>
-        </ScheduleWrapper>
-    );
+                return (
+                  <>
+                    {isTalks && isMonday && (
+                      <TalksPromoInfo rounded pair={tempLesson.lessonNumber} />
+                    )}
+                    <DayLesson key={i} lesson={lesson} />
+                  </>
+                );
+              }
+            )
+          )}
+        </Lessons>
+      </Main>
+    </ScheduleWrapper>
+  );
 }
 
 export default observer(ScheduleDay);
--- src/componets/Views/Schedule/SheduleWeek/SheduleWeek.js
+++ src/componets/Views/Schedule/SheduleWeek/SheduleWeek.js
@@ -1,85 +1,91 @@
-import React, {useEffect} from 'react';
-import './sheduleWeek.scss';
+import React, { useEffect } from "react";
+import "./sheduleWeek.scss";
 import fillEmptyLessons from "../../../../lib/fillEmptyLessons";
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import datesStore from "../../../../store/datesStore";
 import viewModeStore from "../../../../store/viewModeStore";
 import sortLessons from "../../../../lib/sortLessonsByNumber";
 import WeekLesson from "../../../Lesson/WeekLesson/WeekLesson";
-import IconMore from "../../../../assets/img/arrows-expand-right.png"
+import IconMore from "../../../../assets/img/arrows-expand-right.png";
 import {
-    Button, DayLesson,
-    DayLessonsWrapper,
-    DayMonth,
-    Header,
-    Lessons,
-    Main,
-    Meta, ScheduleWrapper
+  Button,
+  DayLesson,
+  DayLessonsWrapper,
+  DayMonth,
+  Header,
+  Lessons,
+  Main,
+  Meta,
+  ScheduleWrapper,
 } from "./ScheduleWeek.style";
 import StateTitle from "../../../Common/NoLessonsTitle";
 import TalksPromoInfo from "../../../Lesson/WeekLesson/TalksPromoInfo";
 
+function ScheduleWeek({ lessons }) {
+  const { setDay, datesWeek, getDatesWeek } = datesStore;
+  const { setView } = viewModeStore;
 
-function ScheduleWeek({lessons}) {
-    const {setDay, datesWeek, getDatesWeek} = datesStore
-    const {setView} = viewModeStore
+  useEffect(() => {
+    (async () => {
+      await getDatesWeek();
+    })();
+  }, [getDatesWeek]);
 
-    useEffect(() => {
-        (async () => {
-            await getDatesWeek()
-        })()
-    }, [getDatesWeek])
+  const generateLessons = (dayLessons, day) => {
+    const fLessons = fillEmptyLessons(dayLessons, day);
+    return fLessons.map((lesson) => {
+      const isMonday = day.weekday === 1;
+      const tempLesson = Array.isArray(lesson) ? lesson[0] : lesson;
+      const isTalks =
+        tempLesson.lessonNumber === 1 || tempLesson.lessonNumber === 4;
 
-    const generateLessons = (dayLessons, day) => {
-        const fLessons = fillEmptyLessons(dayLessons, day);
-        return fLessons.map((lesson) => {
-            const isMonday = day.weekday === 1
-            const isTalks = lesson.lessonNumber === 1 || lesson.lessonNumber === 4
+      return (
+        <>
+          {isTalks && isMonday && <TalksPromoInfo pair={lesson.lessonNumber} />}
+          <WeekLesson key={lesson._id} day={day} lesson={lesson} />
+        </>
+      );
+    });
+  };
 
-            return (<>
-                {isTalks && isMonday && <TalksPromoInfo pair={lesson.lessonNumber}/>}
-                <WeekLesson key={lesson._id} day={day} lesson={lesson}/>
-            </>)
-        });
-    }
+  const changeViewHandle = (date) => {
+    setDay(date.toISODate());
+    setView("day");
+  };
 
-    const changeViewHandle = (date) => {
-        setDay(date.toISODate())
-        setView('day')
-    }
+  return (
+    <ScheduleWrapper>
+      {datesWeek.map((day) => {
+        const dayLessons = sortLessons(
+          lessons.filter((lesson) => lesson.date === day.toISODate())
+        );
+        const dayLessonsComponent =
+          dayLessons.length === 0 ? (
+            <StateTitle>Пар нет</StateTitle>
+          ) : (
+            generateLessons(dayLessons, day)
+          );
 
-
-    return (
-        <ScheduleWrapper>
-            {datesWeek.map(day => {
-                const dayLessons = sortLessons(lessons.filter(lesson => lesson.date === day.toISODate()));
-                const dayLessonsComponent = dayLessons.length === 0
-                    ? <StateTitle>Пар нет</StateTitle>
-                    : generateLessons(dayLessons, day)
-
-                return (
-                    // DayLessonsWrapper max width = 1560px
-                    <DayLessonsWrapper key={day.day}>
-                        <Main>
-                            <Header>
-                                <DayLesson>{day.toFormat('EEEE')}</DayLesson>
-                                <Meta>
-                                    <DayMonth>{day.toFormat("d LLL")}</DayMonth>
-                                    <Button onClick={(() => changeViewHandle(day))}>
-                                        <img src={IconMore} alt="раскрыть"/>
-                                    </Button>
-                                </Meta>
-                            </Header>
-                            <Lessons>
-                                {dayLessonsComponent}
-                            </Lessons>
-                        </Main>
-                    </DayLessonsWrapper>
-                )
-            })}
-        </ScheduleWrapper>
-    );
-
+        return (
+          // DayLessonsWrapper max width = 1560px
+          <DayLessonsWrapper key={day.day}>
+            <Main>
+              <Header>
+                <DayLesson>{day.toFormat("EEEE")}</DayLesson>
+                <Meta>
+                  <DayMonth>{day.toFormat("d LLL")}</DayMonth>
+                  <Button onClick={() => changeViewHandle(day)}>
+                    <img src={IconMore} alt="раскрыть" />
+                  </Button>
+                </Meta>
+              </Header>
+              <Lessons>{dayLessonsComponent}</Lessons>
+            </Main>
+          </DayLessonsWrapper>
+        );
+      })}
+    </ScheduleWrapper>
+  );
 }
 
 export default observer(ScheduleWeek);
