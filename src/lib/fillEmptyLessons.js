3ad117a1f11cd8d0767cac8253134ba1be97213e
Probe1st <svintsitskiy.leha@mail.ru>
2024-05-30 14:15:53 +0500

    change number of lessons
    created variable "numberLessons" that contain number of lessons. If the name of the current division is "№2, Рабочая 43/1", then the variable will get the number 8, else it will get the number 6.

--- src/lib/fillEmptyLessons.js
+++ src/lib/fillEmptyLessons.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 
 export default (dayLessons) => {
+    const numberLessons = dayLessons[0].division.name === '№2, Рабочая 43/1' ? (8) : (6)
 
     const newLessons = [];
     const findLesson = (num, lessons) => {
@@ -16,7 +17,7 @@ export default (dayLessons) => {
         return {subject: {name: "Нет пары"}, lessonNumber: num, _id: _.uniqueId()};
     }
 
-    for (let i = 1; i <= 8; i++) {
+    for (let i = 1; i <= numberLessons; i++) {
         newLessons.push(findLesson(i, dayLessons));
     }
 
