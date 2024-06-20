f93c6abe59dd605667d6b7cd9b8732146887ff62
Probe1st <svintsitskiy.leha@mail.ru>
2024-06-20 16:08:30 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
-export {default as datesStore} from "./datesStore"
-export {default as entrySearch} from "./entrySearch"
-export {default as filterStore} from "./filterStore"
-export {default as scheduleStore} from "./scheduleStore"
-export {default as viewModeStore} from "./viewModeStore"
+export { default as datesStore } from "./datesStore";
+export { default as entrySearch } from "./entrySearch";
+export { default as filterStore } from "./filterStore";
+export { default as scheduleStore } from "./scheduleStore";
+export { default as viewModeStore } from "./viewModeStore";
+export { default as weekStore } from "./weekStore";
