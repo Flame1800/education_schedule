98240898162b2b2273576615b5b998147ec68c45
Probe1st <svintsitskiy.leha@mail.ru>
2024-06-20 16:06:35 +0500

    add WeekStore, WeekSwitcher

--- src/componets/Views/Filter/WeekSwitcher/WeekSwitcher.jsx
+++ src/componets/Views/Filter/WeekSwitcher/WeekSwitcher.jsx
@@ -1,89 +1,98 @@
 import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import scheduleStore from "../../../../store/scheduleStore";
 import { DateTime } from "luxon";
 import {
     ArrowLongLeftIcon,
     ArrowLongRightIcon,
 } from "@heroicons/react/24/solid";
 import datesStore from "../../../../store/datesStore";
+import { getWeek } from "../../../../lib/API";
+import weekStore from "../../../../store/weekStore";
+import Loader from "../../../Loader/Loader";
 
 const WeekSwitcher = () => {
-    const { setCurrentWeek, changeDate, getDate, getCurrentWeek } =
-        scheduleStore;
-    const { setDay, currDay } = datesStore;
+    const { setDay } = datesStore;
+    const { date: weekDate, setDate, setWeek } = weekStore;
     const [dateStartEnd, setDateStartEnd] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     // #region setting week dates
     useEffect(() => {
         const formatDate = (dateISO) =>
-            DateTime.fromISO(dateISO).toFormat("dd.MM.yyyy");
+            dateISO.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}/)?.[0]
+                ? DateTime.fromISO(dateISO).toFormat("dd.MM.yyyy")
+                : "";
 
         (async () => {
             try {
-                await setCurrentWeek();
-                const week = getCurrentWeek();
-                // console.log(week)
+                setIsLoading(true);
 
-                // console.log("before " + currDay)
-                // setDay(currDate.toISODate());
-                // console.log("after " + currDay)
+                const week = await getWeek(weekDate);
+
+                setWeek(week);
+                setDay(weekDate.toISODate());
+
+                const dateString =
+                    formatDate(week?.dateStart ?? "") +
+                    " - " +
+                    formatDate(week?.dateEnd ?? "");
 
                 setDateStartEnd(
-                    formatDate(week?.dateStart ?? currDay) +
-                        " - " +
-                        formatDate(week?.dateEnd ?? currDay)
+                    dateString.length > 3
+                        ? dateString
+                        : weekDate.toFormat("dd.MM.yyyy")
                 );
             } catch (e) {
                 console.error(e);
+            } finally {
+                setIsLoading(false);
             }
         })();
-    }, [getDate()]);
+    }, [weekDate, setDay, setWeek]);
     // #endregion
 
     function decrementWeek() {
-        const currDate  = getDate();
+        const currDate = weekDate;
+        const newDate = DateTime.fromISO(currDate).minus({ week: 1 });
 
-        const newDate = DateTime.fromISO(currDate)
-            .minus({ week: 1 })
-            .toISODate();
-
-        changeDate(newDate);
+        setDate(newDate);
     }
 
     function incrementWeek() {
-        const currDate  = getDate();
+        const currDate = weekDate;
+        const newDate = DateTime.fromISO(currDate).plus({ week: 1 });
 
-        const newDate = DateTime.fromISO(currDate)
-            .plus({ week: 1 })
-            .toISODate();
-
-        changeDate(newDate);
+        setDate(newDate);
     }
 
     return (
-        <div className="flex flex-row items-center justify-center space-x-3 py-1 px-1 h-11 flex-shrink-0">
-            {/* arrow to left */}
-            <div
-                className="flex flex-row justify-center items-center rounded-[30px] borde-none cursor-pointer h-[99%] px-[10px] py-[3px]"
-                onClick={() => decrementWeek()}
-            >
-                <ArrowLongLeftIcon className="w-8 text-gray-500" />
-            </div>
+        <>
+            {isLoading && <Loader />}
+            {!isLoading && (
+                <div className="flex flex-row items-center justify-center space-x-3 py-1 px-1 h-11 flex-shrink-0">
+                    {/* arrow to left */}
+                    <div
+                        className="flex flex-row justify-center items-center rounded-[30px] borde-none cursor-pointer h-[99%] px-[10px] py-[3px]"
+                        onClick={() => decrementWeek()}
+                    >
+                        <ArrowLongLeftIcon className="w-8 text-gray-500" />
+                    </div>
 
-            <div className="flex flex-col items-center">
-                <p className="text-xs">Неделя</p>
-                <p>{dateStartEnd}</p>
-            </div>
+                    <div className="flex flex-col items-center">
+                        <p className="text-xs">Неделя</p>
+                        <p>{dateStartEnd}</p>
+                    </div>
 
-            {/* arrow to right */}
-            <div
-                className="flex flex-row justify-center items-center rounded-[30px] borde-none cursor-pointer h-[99%] px-[10px] py-[3px]"
-                onClick={() => incrementWeek()}
-            >
-                <ArrowLongRightIcon className="w-8 text-gray-500" />
-            </div>
-        </div>
+                    {/* arrow to right */}
+                    <div
+                        className="flex flex-row justify-center items-center rounded-[30px] borde-none cursor-pointer h-[99%] px-[10px] py-[3px]"
+                        onClick={() => incrementWeek()}
+                    >
+                        <ArrowLongRightIcon className="w-8 text-gray-500" />
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
