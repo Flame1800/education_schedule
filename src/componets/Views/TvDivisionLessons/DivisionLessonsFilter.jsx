851ce52add3fe9d197a1c103a7efdfcfe5640150
Probe1st <svintsitskiy.leha@mail.ru>
2024-07-09 16:21:13 +0500

    add the circle loader to the loading of divisions and refactoring the code

--- src/componets/Views/TvDivisionLessons/DivisionLessonsFilter.jsx
+++ src/componets/Views/TvDivisionLessons/DivisionLessonsFilter.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import FilterParam from "../Filter/FillterTabs/FilterParams/FilterParam";
-import filterStore from "../../../store/filterStore";
 import { Link } from "react-router-dom";
 import "../Filter/filter.scss";
 import { observer } from "mobx-react-lite";
@@ -12,20 +11,37 @@ import {
     ShowAllGroupsBtn,
 } from "../Filter/Filter.style";
 import { Column, FilterItems } from "../Filter/FillterTabs/FilterTabs.styled";
-import scheduleStore from "../../../store/scheduleStore";
 import { DateTime } from "luxon";
 import styled from "styled-components";
-import datesStore from "../../../store/datesStore";
 import { beautyDate } from "../../../lib/beautyDate";
+import {
+    datesStore,
+    filterStore,
+    scheduleStore,
+    weekStore,
+} from "../../../store";
+import CircleLoader from "../../CircularLoader/CircularLoader";
 import Skeleton from "react-loading-skeleton";
 
 const DivisionLessonsFilter = () => {
-    const { loading } = scheduleStore;
+    const [loading, setLoading] = useState(false);
     const { setDay } = datesStore;
+    const { divisions, getDivisions } = filterStore;
+    const { week } = weekStore;
 
     useEffect(() => {
         setDay(DateTime.now().toISODate());
     }, [setDay]);
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+
+            await getDivisions().finally(() => {
+                setLoading(false);
+            });
+        })();
+    }, [week, getDivisions]);
 
     const renderBackButton = () => (
         <Link to="/timetable">
@@ -39,30 +55,48 @@ const DivisionLessonsFilter = () => {
         </Date>
     );
 
+    const dateLoader = (
+        <Date>
+            <Skeleton width={"10rem"} />
+        </Date>
+    );
+
+    const filterItemsLoader = (
+        <FilterItems>
+            <Column>
+                <CircleLoader className="h-24" />
+            </Column>
+        </FilterItems>
+    );
+
     return (
         <FilterWrapper>
             <div>
                 <Title>Выберите корпус</Title>
-                <FilterItems>
-                    <Column>
-                        {filterStore.divisions.map((item) => {
-                            return (
-                                <Link
-                                    key={item._id}
-                                    to={`/timetable/divisions/${encodeURIComponent(
-                                        item.name
-                                    )}?week=curr`}
-                                >
-                                    <FilterParam item={item} />
-                                </Link>
-                            );
-                        })}
-                    </Column>
-                </FilterItems>
+                {loading ? (
+                    filterItemsLoader
+                ) : (
+                    <FilterItems>
+                        <Column>
+                            {divisions.map((item) => {
+                                return (
+                                    <Link
+                                        key={item._id}
+                                        to={`/timetable/divisions/${encodeURIComponent(
+                                            item.name
+                                        )}?week=curr`}
+                                    >
+                                        <FilterParam item={item} />
+                                    </Link>
+                                );
+                            })}
+                        </Column>
+                    </FilterItems>
+                )}
             </div>
             <BlockInfo>
                 {/* TODO: сделать лоадер */}
-                {loading ? <Skeleton /> : renderDate()}
+                {loading ? dateLoader : renderDate()}
 
                 {renderBackButton()}
             </BlockInfo>
